48c50aa9ad39552dd55f6f8b1ab7a8d36cea26a3
Arkady Palver <arkadypalver@gmail.com>
2016-12-13 23:30:26 +0100

    Delete nvidia.sh

--- nvidia.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#Controlador propietario de Nvidia instalar para Debian 8 Jessie
-
-sudo apt remove xserver-xorg-video-nouveau
-
-sudo apt install -y linux-headers-$(uname -r|sed 's,[^-]*-[^-]*-,,') nvidia-kernel-dkms nvidia-glx nvidia-kernel-common nvidia-xconfig nvidia-settings nvidia-vdpau-driver libgl1-nvidia-glx libgl1-nvidia-glx-i386 libtxc-dxtn-s2tc0 libtxc-dxtn-s2tc-bin libtxc-dxtn-s2tc0:i386 nvidia-cg-toolkit uuid-runtime
-
-sudo nvidia-xconfig
